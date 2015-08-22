ddd3237161e4b72a3c7c87007260cb70b060e68f
Radoslav Petkov <radoslav.petkov@sirma.bg>
2015-08-22 17:29:49 +0300

    Continuous testing workflow

--- test/models/people/dev.spec.ts
+++ test/models/people/dev.spec.ts
@@ -4,9 +4,9 @@ let expect = chai.expect;
 
 describe('Tests for the dev',function(){
    it('Test if .info() gives correct info',function(){
-      let dev : Dev = new Dev('Jack');
+      let dev : Dev = new Dev('Jack1');
 
-    expect(dev.info()).to.equal('Jack');
+    expect(dev.info()).to.equal('Jack1');
 
    });
 });
